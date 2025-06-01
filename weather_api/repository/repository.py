a808e12999574697bea096e38284c718e2add149
Jamie Kohns <jamiekohns@gmail.com>
2025-06-01 17:17:40 +0000

    more extraction into base repo class

--- weather_api/repository/repository.py
+++ weather_api/repository/repository.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 class Repository:
     def __init__(self, base_url=None):
@@ -9,6 +10,22 @@ class Repository:
             'units': os.getenv("API_UNITS", default=""),
         }
 
-    def setOptions(self, options=None):
-        options = options or {}
-        self.options.update(options)
+    def send(self, url, method, params=None):
+        params = params or {}
+
+        # merge our passed params with defautl options
+        self.options.update(params)
+        
+        # prepend our base_url   
+        url = f"{self.base_url}/{url}"
+
+        try:
+            response = requests.request(method, url, params=self.options)
+            response.raise_for_status()
+
+            data = response.json()
+        except requests.RequestException as e:
+            print(f"API error: {e}")
+            return None
+
+        return data
--- weather_api/repository/weather_repository.py
+++ weather_api/repository/weather_repository.py
@@ -1,4 +1,3 @@
-import requests
 import os
 from weather_api.models import Weather
 from var_dump import var_dump
@@ -6,19 +5,6 @@ from .repository import Repository
 
 class WeatherRepository(Repository):
     def get_current_weather(self, options=None):
-        url = f"{self.base_url}/weather"
-        self.setOptions(options)
-
-        try:
-            response = requests.get(url, params=self.options)
-            response.raise_for_status()
-
-            data = response.json()
-            return Weather.fromApi(data)
-
-        except requests.RequestException as e:
-            print(f"Weather API error: {e}")
-            return None
-
-    def create_forecast(self, forcast):
-        requests.request('get')
+        data = self.send('/weather', 'get', options)
+        
+        return Weather.fromApi(data)
